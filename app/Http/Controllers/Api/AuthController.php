ab93ebec97d5387717324063a65a8209d44900ac
jonnathan <jonnabcl56@gmail.com>
2026-04-08 10:59:05 -0500

    feat: agregar validación para confirmación de contraseña en el registro de usuario

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -18,6 +18,7 @@ class AuthController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8|confirmed',
+            'confirm_password' => 'required|string|min:8|same:password',
         ]);
 
         $user = User::create([
